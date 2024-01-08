5affbb033ae918f9ad1d17a216c809b6b25b7639
Henrique Pinheiro <henrique@gampoc.com>
2024-01-08 09:16:53 -0300

    Initial commit

---
+++ src/i18n/strings.ts
@@ -0,0 +1,72 @@
+export const languages = {
+  en: 'English',
+  pt: 'Português (Brasileiro)'
+}
+
+export const defaultLang = 'en'
+
+export const strings = {
+  en: {
+    'title.resume': 'Resume',
+    'header.jobTitle': 'Frontend Tech Lead',
+    'main.workExperience': 'Work Experience',
+    'job.1.title': 'Frontend Team Lead',
+    'job.1.company': 'Toggl',
+    'job.1.from': 'Jul 2020',
+    'job.1.to': 'Present',
+    'job.1.description': `Facilitated extensive discussions and collaborations across multiple teams, ensuring alignment and efficient project delivery.
+    Actively contributed to technical discussions and design specifications, influencing key project decisions`,
+    'job.2.title': 'Cross functional Team Lead',
+    'job.2.company': 'Toggl',
+    'job.2.from': 'Jan 2020',
+    'job.2.to': 'Jul 2020',
+    'job.2.description': `Managed a cross-function team tasked with redesigning our data sync strategy.
+    We solved long standing issues in poor network conditions across all of our clients, with redesigned endpoints and data syncing strategy`,
+    'job.3.title': 'Frontend Developer',
+    'job.3.company': 'Toggl',
+    'job.3.from': 'Mar 2017',
+    'job.3.to': 'Jan 2020',
+    'job.3.description': `Maintained and improved our main web application in a large monorepo using React and Redux.
+    Engaged in continuous improvement of the development process, focusing on simplicity and developer experience`,
+    'job.4.title': 'Partner',
+    'job.4.company': 'HD Informática Ltda',
+    'job.4.from': 'Sep 2007',
+    'job.4.to': 'Present',
+    'job.4.description': `Created customized solutions to meet the unique needs of business partners, using a wide range of technology stacks including PHP, MySQL, Wordpress, NodeJS, Angular, and React`,
+    'q.aboutMe': 'About me',
+    'q.aboutMe.description': `Frontend Team Lead and Developer with over 17 years of experience in engineering and management. Passionate about creating simple, effective UI/UX and leading teams to deliver innovative solutions. Committed to continuous improvement, efficient team communication, and fostering a collaborative work environment`,
+    'q.education': 'Education',
+    'q.education.title': 'Bachelor of Computer Engineering',
+    'q.skills': 'Skills',
+    'q.skills.1': 'Portuguese (Native)',
+    'q.skills.2': 'English (Fluent CEFR C2)',
+    'q.skills.3': 'Project management'
+  },
+  pt: {
+    'title.resume': 'Currículo',
+    'header.jobTitle': 'Tech Lead de Frontend',
+    'main.workExperience': 'Experiência Profissional',
+    'job.1.title': 'Team Lead de Frontend',
+    'job.1.to': 'Presente',
+    'job.1.description': `Facilitei discussões e colaborações entre múltiplas equipes, garantindo alinhamento e entrega eficiente de projetos.
+    Contribuí ativamente para discussões técnicas e especificações de design, influenciando decisões-chave de projetos`,
+    'job.2.title': 'Team Lead Multifucional',
+    'job.2.description': `Gerenciei uma equipe multifuncional encarregada de redesenhar nossa estratégia de sincronização de dados.
+    Resolvemos problemas de longa data em condições de rede ruins em todos os nossos aplicativos cliente, redesenhando endpoints e a estratégia de sincronização de dados`,
+    'job.3.title': 'Desenvolvedor Frontend',
+    'job.3.description': `Mantive e melhorei nossa aplicação web principal em um grande monorepo usando React e Redux.
+    Trabalhei na melhoria contínua do processo de desenvolvimento, focando em simplicidade e experiência do desenvolvedor`,
+    'job.4.title': 'Sócio',
+    'job.4.from': 'Set 2007',
+    'job.4.to': 'Presente',
+    'job.4.description': `Criei soluções personalizadas para atender às necessidades únicas de parceiros de negócios, usando uma ampla gama de tecnologias, incluindo PHP, MySQL, Wordpress, NodeJS, Angular e React`,
+    'q.aboutMe': 'Sobre mim',
+    'q.aboutMe.description': `Team Lead e Desenvolvedor Frontend com mais de 17 anos de experiência em engenharia e gestão. Apaixonado por criar UI/UX simples e eficazes e liderar equipes para entregar soluções inovadoras. Comprometido com a melhoria contínua, comunicação eficiente da equipe e promoção de um ambiente de trabalho colaborativo`,
+    'q.education': 'Formação',
+    'q.education.title': 'Bacharelado em Engenharia da Computação',
+    'q.skills': 'Competências',
+    'q.skills.1': 'Português (Nativo)',
+    'q.skills.2': 'Inglês (Fluente CEFR C2)',
+    'q.skills.3': 'Gestão de projetos'
+  }
+} as const
