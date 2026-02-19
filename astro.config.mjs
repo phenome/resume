e876cf0577ce3c2931be7f1fff86287969dda67b
Henrique Pinheiro <henrique@gampoc.com>
2026-02-19 17:39:48 -0300

    feat(pages): deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://phenome.github.io',
+  base: isGitHubActions ? '/resume' : '/',
   integrations: [tailwind()],
 });
